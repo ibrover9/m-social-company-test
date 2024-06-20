5cb4846a8ba62636845f0e2e865e3db44a2f52cf
Artur Novikov <artur.20177102@mail.ru>
2024-06-20 23:10:49 +0300

    I made the main Registration component (added inputs and labels). I also developed styles.

--- components/mainRegistration/index.tsx
+++ components/mainRegistration/index.tsx
@@ -1,3 +1,49 @@
+import style from "./style.module.scss";
+
 export default function MainRegistration() {
-  return <div>GGG</div>;
+  return (
+    <>
+      <div className={style.block_recording_data}>
+        <label htmlFor="user-name">Имя</label>
+        <input type="text" id="user-name" name="user-name" />
+      </div>
+
+      <div className={style.block_recording_data}>
+        <label className={style.label} htmlFor="user-city">
+          Ваш город
+        </label>
+        <input type="text" id="user-city" name="user-city" />
+      </div>
+      <hr />
+      <div className={style.block_recording_data}>
+        <label htmlFor="user-password">Пароль</label>
+        <input type="text" id="user-password" name="user-password" />
+      </div>
+      <div className={style.block_recording_data}>
+        <label htmlFor="repeat-password">Пароль еще раз</label>
+        <input type="text" id="repeat-password" name="repeat-password" />
+      </div>
+      <hr />
+      <div className={style.block_recording_data}>
+        <label htmlFor="user-telephone">Номер телефона</label>
+        <input type="text" id="user-telephone" name="user-telephone" />
+      </div>
+      <div className={style.block_recording_data}>
+        <label htmlFor="user-Email">Электронная почта</label>
+        <input type="text" id="user-Email" name="user-Email" />
+      </div>
+      <div className={style.block_recording_data}>
+        <label htmlFor="user-Email">Электронная почта</label>
+        <input type="checkbox" id="user-Email" name="user-Email" />
+        <span>принимать актуальную информацию на email</span>
+      </div>
+      <div className={style.button_container}>
+        <input
+          className={style.button_change_form}
+          type="button"
+          value="Изменить"
+        />
+      </div>
+    </>
+  );
 }
