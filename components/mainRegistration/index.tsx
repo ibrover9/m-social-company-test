04ba0f3284a19632ca75789df40366d52ebf649a
Artur Novikov <artur.20177102@mail.ru>
2024-06-21 12:30:32 +0300

    add placeholder and style him

--- components/mainRegistration/index.tsx
+++ components/mainRegistration/index.tsx
@@ -5,32 +5,69 @@ export default function MainRegistration() {
     <>
       <div className={style.block_recording_data}>
         <label htmlFor="user-name">Имя</label>
-        <input type="text" id="user-name" name="user-name" />
+        <input
+          type="text"
+          placeholder="Введите имя"
+          id="user-name"
+          name="user-name"
+        />
       </div>
 
       <div className={style.block_recording_data}>
         <label className={style.label} htmlFor="user-city">
           Ваш город
         </label>
-        <input type="text" id="user-city" name="user-city" />
+        <input
+          placeholder="Выберите город"
+          list="user-cities"
+          id="user-city"
+          name="city"
+        />
+        <datalist id="user-cities">
+          <option value="Chrome" />
+          <option value="Firefox" />
+          <option value="Safari" />
+          <option value="Edge" />
+          <option value="Opera" />
+        </datalist>
       </div>
       <hr />
       <div className={style.block_recording_data}>
         <label htmlFor="user-password">Пароль</label>
-        <input type="text" id="user-password" name="user-password" />
+        <input
+          placeholder="Введите Пароль"
+          type="text"
+          id="user-password"
+          name="user-password"
+        />
       </div>
       <div className={style.block_recording_data}>
         <label htmlFor="repeat-password">Пароль еще раз</label>
-        <input type="text" id="repeat-password" name="repeat-password" />
+        <input
+          placeholder="Повторите Пароль"
+          type="text"
+          id="repeat-password"
+          name="repeat-password"
+        />
       </div>
       <hr />
       <div className={style.block_recording_data}>
         <label htmlFor="user-telephone">Номер телефона</label>
-        <input type="text" id="user-telephone" name="user-telephone" />
+        <input
+          placeholder="+7 (***) ***-**-**"
+          type="tel"
+          id="user-telephone"
+          name="user-telephone"
+        />
       </div>
       <div className={style.block_recording_data}>
         <label htmlFor="user-Email">Электронная почта</label>
-        <input type="text" id="user-Email" name="user-Email" />
+        <input
+          placeholder="Введите email"
+          type="text"
+          id="user-Email"
+          name="user-Email"
+        />
       </div>
       <div className={style.block_recording_data}>
         <label htmlFor="user-consent">Я согласен</label>
