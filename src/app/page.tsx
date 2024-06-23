356b886c5ddbee94bddbc4edc805ec8b471b1a99
Artur Novikov <artur.20177102@mail.ru>
2024-06-23 13:10:36 +0300

    (asyncFunctions, components, constants, functions, models) in src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import style from "../app/style.module.scss";
-import Registration from "../../components/Registration/index";
+import Registration from "../components/Registration/index";
 
 export default function Home() {
   return <Registration />;
