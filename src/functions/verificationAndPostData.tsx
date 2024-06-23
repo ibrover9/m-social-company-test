d2b8cad56a6cbc6e6fa5529dfc47e7c52d9a10b1
Artur Novikov <artur.20177102@mail.ru>
2024-06-23 22:56:28 +0300

    update validation

--- src/functions/verificationAndPostData.tsx
+++ src/functions/verificationAndPostData.tsx
@@ -37,39 +37,42 @@ export function verificationAndPostData(
   setIsButtonDisabled(true);
   let item = "Ошибка в имени";
   if (allCyrillic(name) && checkMinTwoElements(name)) {
-    item = "Ошибка в паролях";
-    if (password == repeatPassword) {
-      item = "Ошибка с номером";
-      if (checkLastCharacterNumber(number)) {
-        // item = "Ошибок в пунктах нет";
-        item = "Некорректная почта";
-        if (checkBox) {
-          if (isValidEmail) {
-            item = "Все отправлено";
-            postForm(
-              name,
-              password,
-              number,
-              city,
-              email,
-              setIsButtonDisabled,
-              setCleaningFormState
-            );
-            newDateAndTime(setDateAndTime);
-          }
-        } else {
-          if (isValidEmail || email == "") {
-            item = "Все отправлено";
-            postForm(
-              name,
-              password,
-              number,
-              city,
-              email,
-              setIsButtonDisabled,
-              setCleaningFormState
-            );
-            newDateAndTime(setDateAndTime);
+    item = "Не указан город";
+    if (city != "") {
+      item = "Ошибка в паролях";
+      if (password == repeatPassword && password != "") {
+        item = "Ошибка с номером";
+        if (checkLastCharacterNumber(number)) {
+          // item = "Ошибок в пунктах нет";
+          item = "Некорректная почта";
+          if (checkBox) {
+            if (isValidEmail) {
+              item = "Все отправлено";
+              postForm(
+                name,
+                password,
+                number,
+                city,
+                email,
+                setIsButtonDisabled,
+                setCleaningFormState
+              );
+              newDateAndTime(setDateAndTime);
+            }
+          } else {
+            if (isValidEmail || email == "") {
+              item = "Все отправлено";
+              postForm(
+                name,
+                password,
+                number,
+                city,
+                email,
+                setIsButtonDisabled,
+                setCleaningFormState
+              );
+              newDateAndTime(setDateAndTime);
+            }
           }
         }
       }
